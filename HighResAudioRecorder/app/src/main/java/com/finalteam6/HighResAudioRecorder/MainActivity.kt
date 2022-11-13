1fd16fa93a51639f41e063381db9e60c3e4a5352
Benjamin Hoang <benjaminh564@gmail.com>
2022-11-13 10:43:25 -0800

    added bottom navigation

--- HighResAudioRecorder/app/src/main/java/com/finalteam6/HighResAudioRecorder/MainActivity.kt
+++ HighResAudioRecorder/app/src/main/java/com/finalteam6/HighResAudioRecorder/MainActivity.kt
@@ -2,6 +2,8 @@ package com.finalteam6.HighResAudioRecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity()
 {
@@ -10,6 +12,40 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide();
         setContentView(R.layout.activity_main)
+
+        val recordFrag = fragment_record()
+        val settingsFrag = fragment_settings()
+        val recordingsFrag = fragment_showrecordings()
+
+        changeFragment(recordFrag)
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnItemSelectedListener {
+                item ->
+            when(item.itemId){
+                R.id.record ->{
+                    changeFragment(recordFrag)
+                    true
+                }
+                R.id.display->{
+                    changeFragment(recordingsFrag)
+                    true
+                }
+                R.id.settings->{
+                    changeFragment(settingsFrag)
+                    true
+                }
+                else->{
+                    false
+                }
+            }
+        }
+
+    }
+    private fun changeFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView,fragment)
+            commit()
+        }
+
 
         //
     }
