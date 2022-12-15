902cbbde13fc4490e72dc523a08c40ce6347d1dd
Jarrett Koran <jarrettkoran@gmail.com>
2022-12-15 15:47:56 -0800

    Unit Tests Begining

---
+++ HighResAudioRecorder/app/src/test/java/com/finalteam6/HighResAudioRecorder/fragment_recordTest.kt
@@ -0,0 +1,29 @@
+package com.finalteam6.HighResAudioRecorder
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class fragment_recordTest{
+
+
+    @Test
+    fun RecordClick(){
+        //assertTrue()
+    }
+    @Test
+    fun AttemptToStartTest(){
+        //assertTrue(recorderTest.attemptToStartRecording())
+    }
+    @Test
+    fun AttemptToStopTest(){
+        //assertTrue(recorderTest.attemptToStartRecording())
+    }
+    @Test
+    fun PermissionWarnTest(){
+        //assertTrue(recorderTest.attemptToStartRecording())
+    }
+
+}
